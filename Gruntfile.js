6c7d6b63c4135cbd3025ab4c0f262c275434bde0
Pierre Rudloff <contact@rudloff.pro>
2016-09-26 13:40:14 +0200

    We don't need the shipit:staging-redirect task anymore

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,9 +27,6 @@ module.exports = function (grunt) {
                 deployTo: 'mediawiki-beta/',
                 branch: 'develop'
             },
-            'staging-redirect': {
-                deployTo: 'mediawiki-beta/redirect/'
-            },
             prod: {
                 deployTo: 'mediawiki/',
                 branch: 'master'
@@ -51,6 +48,6 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('lint', ['jslint', 'jsonlint', 'fixpack']);
-    grunt.registerTask('staging', ['shipit:staging', 'update', 'composer:install', 'composer:cmd', 'shipit:staging-redirect', 'composer:install']);
+    grunt.registerTask('staging', ['shipit:staging', 'update', 'composer:install', 'composer:cmd']);
     grunt.registerTask('prod', ['shipit:prod', 'update', 'composer:install', 'composer:cmd']);
 };
